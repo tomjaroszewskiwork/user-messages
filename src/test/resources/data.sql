2f9699cf30ffd60d3e888eedf399c6957cd7002a
Tom Jaroszewski <tomjaroszewski@tradingcentral.com>
2018-08-22 08:48:55 -0400

    Added test data

---
+++ src/test/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO message (user_id, message_id, message, generated_at) VALUES
+  ('tom.j', 100, 'test string', '2018-08-08 20:08:08-4:00'),
+  ('tom.j', 101, 'lool', '2018-08-08 20:08:09-4:00'),
+  ('tom.j', 102, 'longer message filled with words', '2018-10-08 20:08:09-4:00'),
+  ('bob.dole', 103, 'secret words secret', '2018-10-08 20:08:09-4:00');
